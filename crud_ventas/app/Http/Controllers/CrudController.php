e2ed5cc550ab324a39ac9c65fbf5b766f05ebfc2
nico-2004 <nicoanear@gmail.com>
2023-06-16 09:58:50 -0500

    crud de ventas

---
+++ crud_ventas/app/Http/Controllers/CrudController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\Task\StoreRequest;
+use App\Models\Crud;
+use Illuminate\Http\Request;
+
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cruds = Crud::all();
+        return view('Crud.index',compact('cruds')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->only('name','description','date');
+        Crud::created($data);
+        return redirect()->back()->with('completo','registro completado');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Crud $crud)
+    {
+        //
+    }
+}
