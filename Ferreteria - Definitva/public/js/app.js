683e355927a94ebec6cb70eb9379cbb197d6d010
danielllg10 <125503302+danielllg10@users.noreply.github.com>
2023-07-04 09:39:01 -0500

    Add files via upload

---
+++ Ferreteria - Definitva/public/js/app.js
@@ -0,0 +1,23 @@
+import './bootstrap';
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+const imagenes = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.todos');
+const btnEnsaladas = document.querySelector('.ensaladas');
+const btnPasta = document.querySelector('.pasta');
+const btnPizza = document.querySelector('.pizza');
+const btnPostres = document.querySelector('.postres');
+const contenedorPlatillos = document.querySelector('.platillos');
+document.addEventListener('DOMContentLoaded',()=>{
+    eventos();
+    platillos();
+});
+
+const eventos = () =>{
+    menu.addEventListener('click',abrirMenu);
+}
+
+const abrirMenu = () =>{
+     navegacion.classList.remove('ocultar');
+     botonCerrar();
+}
